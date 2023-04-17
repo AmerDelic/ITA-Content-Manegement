fecb184a268427e3da25c92bc41000a04eede138
Amer Delić <amer.delic@qenta.com>
2023-04-17 14:27:38 +0200

    Initial Commit

---
+++ src/main/java/com/example/itacontentmanagement/ItaContentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.itacontentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItaContentManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItaContentManagementApplication.class, args);
+    }
+
+}
